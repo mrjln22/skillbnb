94ac7101a9255fd8b94b524a76b1ff19376aae31
george woods <georgewoods01@gmail.com>
2022-05-31 12:28:00 +0200

    added create and private methods

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,4 +4,19 @@ class ServicesController < ApplicationController
 
   def show
   end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:skill_name, :price_per_hour, :availability)
+  end
 end
