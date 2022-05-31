a00158ae598449d52faf91a90f00e5fa0149fc1f
george woods <georgewoods01@gmail.com>
2022-05-31 15:48:27 +0200

    added 5 seeds for users, services, and bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "deleting all users"
+
+User.destroy_all
+
+User.create!(email: "george@gmail.com", password: "123456", first_name: "George", last_name: "Woods", role: "teacher")
+User.create!(email: "marlon@gmail.com", password: "123456", first_name: "Marlon", last_name: "Wanderllich", role: "student")
+User.create!(email: "ricardo@gmail.com", password: "123456", first_name: "Ricardo", last_name: "Mil", role: "student")
+User.create!(email: "marjolijn@gmail.com", password: "123456", first_name: "Marjolijn", last_name: "Van der Ree", role: "teacher")
+User.create!(email: "ivo@gmail.com", password: "123456", first_name: "Ivo", last_name: "Something", role: "student")
+
+puts "creating all users again"
+
+puts "deleting all services"
+
+Service.destroy_all
+
+Service.create!(skill_name: "Underwater basket weaving", price_per_hour: 75, availability: true, user: User.last)
+Service.create!(skill_name: "Gardening tips", price_per_hour: 150, availability: true, user: User.last)
+Service.create!(skill_name: "Esperanto classes", price_per_hour: 20, availability: true, user: User.last)
+Service.create!(skill_name: "Pogo stick for advanced learners", price_per_hour: 400, availability: true, user: User.last)
+Service.create!(skill_name: "Korean cooking lessons", price_per_hour: 10, availability: true, user: User.last)
+
+puts "creating services again"
+
+puts "deleting all bookings"
+
+Booking.destroy_all
+
+Booking.create!(status: "pending", user: User.last, service: Service.last)
+Booking.create!(status: "confirmed", user: User.last, service: Service.last)
+Booking.create!(status: "cancelled", user: User.last, service: Service.last)
+Booking.create!(status: "confirmed", user: User.last, service: Service.last)
+Booking.create!(status: "pending", user: User.last, service: Service.last)
+
+puts "creating bookings again"
