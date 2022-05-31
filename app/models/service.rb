cb528ff5740ee6ad9847479568a704b7114ba539
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-05-31 10:36:37 +0100

    added service validations

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,6 @@
 class Service < ApplicationRecord
   belongs_to :user
+  validates :skill_name, presence: true
+  validates :price_per_hour, presence: true
+  validates :availability, presence: true
 end
