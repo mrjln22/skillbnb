3bf4c5de01b46e77d36ebf13721e53040d2a76e3
ricardo milhomens <ricardo.m.mil@hotmail.com>
2022-06-02 11:04:08 +0100

    Add services cards to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def index
     @bookings = current_user.bookings_as_teacher
+    @student_booking = Booking.where(user: current_user)
+    @services = Service.where(user: current_user)
   end
 end
